bb036fa8c3998c164e63fb19ffd23f490b157ada
Justdhif <noob1234five@gmail.com>
2024-09-08 17:48:39 +0700

    link

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+const htmlBtn = document.getElementById('html');
+const cssBtn = document.getElementById('css');
+
+htmlBtn.addEventListener('click', () => {
+    document.getElementById('html_content').style.display = 'flex';
+    document.getElementById('css_content').style.display = 'none';
+
+    htmlBtn.classList.add('active');
+    cssBtn.classList.remove('active');
+})
+
+cssBtn.addEventListener('click', () => {
+    document.getElementById('html_content').style.display = 'none';
+    document.getElementById('css_content').style.display = 'flex';
+
+    htmlBtn.classList.remove('active');
+    cssBtn.classList.add('active');
+})
